429e5372c3e016706a37e6d9b29705a497344799
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-11-28 16:02:22 +0100

    mise en place structure du code

---
+++ frontend/src/componants/nav/horizontal/NavHorizontal.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavHorizontal = () => {
+  return <div></div>;
+};
+
+export default NavHorizontal;
---
+++ frontend/src/componants/nav/vertical/NavVertical.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const NavVertical = () => {
+  return <div></div>;
+};
+
+export default NavVertical;
