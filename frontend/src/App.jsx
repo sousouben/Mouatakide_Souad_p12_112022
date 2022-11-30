e55e3de5e67875c305f94c57dfb447140276c570
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-11-30 18:14:25 +0100

    début de la page home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <NavHorizontal />
       <NavVertical />
       <Routes>
-        <Route path="/user/:id" element={<Home />} />
+        <Route path="/" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
