179c41cc0853ba9ff51aacf49aa8ec9027808094
SOUSOUBENH <75803460+sousouben@users.noreply.github.com>
2022-12-01 12:52:44 +0100

    ajout routes dans app.jsx

--- Backend/app/routes.js
+++ Backend/app/routes.js
@@ -1,50 +1,48 @@
-const express = require('express')
-const idx = require('idx')
+const express = require("express");
+const idx = require("idx");
 
-const router = express.Router()
-
-const {
-    getUserById,
-    getUserActivityById,
-    getUserAverageSession,
-    getUserPerformance
-} = require('./models')
+const router = express.Router();
 
 const {
-    handleNoUserData
-} = require('./middleware')
+  getUserById,
+  getUserActivityById,
+  getUserAverageSession,
+  getUserPerformance,
+} = require("./models");
 
+const { handleNoUserData } = require("./middleware");
 
-router.get('/user/:id', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserById(Number(userId))
+//Cette route me donne des informations générales sur l’utilisateur.
+router.get("/user/:id", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserById(Number(userId));
 
-    return handleNoUserData(res, userData)
-})
+  return handleNoUserData(res, userData);
+});
 
+//Cette route donne des informations sur le poids et les calories brûlées.
+//informations sur les calories, protéines, glucides et lipides de la journée.
+router.get("/user/:id/activity", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserActivityById(Number(userId));
 
-router.get('/user/:id/activity', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserActivityById(Number(userId))
+  return handleNoUserData(res, userData);
+});
 
-    return handleNoUserData(res, userData)
-})
+//Cette route donne des informations sur la durée moyenne des sessions.
+router.get("/user/:id/average-sessions", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserAverageSession(Number(userId));
 
+  return handleNoUserData(res, userData);
+});
 
-router.get('/user/:id/average-sessions', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserAverageSession(Number(userId))
+//Cette route me donne des informations pour le radar chart.
+router.get("/user/:id/performance", (req, res) => {
+  const userId = idx(req, (_) => _.params.id);
+  const userData = getUserPerformance(Number(userId));
 
-    return handleNoUserData(res, userData)
-})
+  return handleNoUserData(res, userData);
+});
 
-
-router.get('/user/:id/performance', (req, res) => {
-    const userId = idx(req, _ => _.params.id)
-    const userData = getUserPerformance(Number(userId))
-
-    return handleNoUserData(res, userData)
-})
-
-
-module.exports = router
+module.exports = router;
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,7 +10,7 @@ const App = () => {
       <NavHorizontal />
       <NavVertical />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route path="/user/:id" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
